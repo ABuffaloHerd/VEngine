e2feb0d514cdc8010188aefc5a7407a6b5563d15
ABuffaloHerd <blaster140902@gmail.com>
2023-11-03 11:56:43 +1300

    replace the old project with a new one since the old one broke during the update

---
+++ Void-sc10/Program.cs
@@ -0,0 +1,11 @@
+﻿Settings.WindowTitle = "My SadConsole Game";
+
+Game.Configuration gameStartup = new Game.Configuration()
+    .SetScreenSize(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+    .SetStartingScreen<Void_sc10.Scenes.RootScene>()
+    .ConfigureFonts((f) => f.UseCustomFont("Resources/Font/Cheepicus12.font"))
+    ;
+
+Game.Create(gameStartup);
+Game.Instance.Run();
+Game.Instance.Dispose();
